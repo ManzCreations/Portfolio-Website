52601740627a333122fa82b0cd6c6289f5108b42
Steven Manz <128404387+ManzCreations@users.noreply.github.com>
2026-02-24 20:42:54 -0500

    Committed for Render Use

--- app.py
+++ app.py
@@ -191,4 +191,5 @@ def _add_vwap(df: pd.DataFrame) -> pd.DataFrame:
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
